f308564671dd67a92cdae4e069bcc262c52ae824
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2021-02-08 10:54:13 +0530

    spellfix

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('Its working')
-
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
@@ -14,7 +12,7 @@ let themeDots = document.getElementsByClassName('theme-dot')
 for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
+		console.log('bruh', mode)
 		setTheme(mode)
 	})
 }
